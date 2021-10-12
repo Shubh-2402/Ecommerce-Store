3488c7f4b1c14cbea34a68b81078c5b26f463a08
Shubh-2402 <56020434+Shubh-2402@users.noreply.github.com>
2021-10-13 00:06:38 +0530

    separated production and development errors

--- backend/middlwares/errors.js
+++ backend/middlwares/errors.js
@@ -1,13 +1,30 @@
 import ErrorHandler from "../utils/errorHandler.js";
 
 const errorMiddleware = (err,req,res,next) => {
-    err.statusCode = err.statusCode || 500
-    err.message = err.message || "Internal Server Error"
 
-    res.status(err.statusCode).json({
-        success: false,
-        error: err.stack
-    })
+    err.statusCode = err.statusCode || 500
+
+    if(process.env.NODE_ENV.trim() === 'DEVELOPMENT'){
+
+        res.status(err.statusCode).json({
+            success:false,
+            error:err,
+            message:err.message,
+            stack:err.stack
+        })
+    }
+
+     if(process.env.NODE_ENV.trim() === 'PRODUCTION'){
+
+        let error = {...err}
+        error.message = err.message
+
+        res.status(error.statusCode).json({
+            success: false,
+            message: error.message || "Internal Server Error"
+        })
+    }
+
 }
 
 export default errorMiddleware
