695770c7867a530e5c0d1ace9ffb81e830a793ba
Shubh-2402 <56020434+Shubh-2402@users.noreply.github.com>
2021-10-17 20:09:49 +0530

    added user model

---
+++ backend/models/user.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose"
+import validator from "validator"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+// import dotenv from "dotenv"
+// dotenv.config({ path: 'backend/config/config.env' })
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please enter your name"],
+        trim:true,
+        maxlength:[30,"Name cannot exceed 300 characters"]
+    },
+    email:{
+        type:String,
+        required:[true,"Please enter your name"],
+        unique:true,
+        validate:[validator.isEmail,"Please enter valid email address"]
+    },
+    password:{
+        type:String,
+        required:[true,"Please enter your password"],
+        minlength:[8,"Password must be atleast 8 characters long"],
+        select:false
+    },
+    avatar:{
+
+        public_id:{
+            type:String,
+            required:true
+        },
+        url:{
+            type:String,
+            required:true
+        }
+    },
+    role:{
+        type:String,
+        default:"user"
+    },
+    
+    resetPasswordToken:String,
+    resetPasswordExpire:Date
+
+},{timestamps:true})
+
+
+// Encrypting password before saving
+
+userSchema.pre("save", async function (next){
+
+    if(!this.isModified("password")){
+        next()
+    }
+
+    this.password = await bcrypt.hashSync(this.password,10);
+})
+
+
+// Return JWT token 
+
+userSchema.methods.getJwtToken = function (){
+
+    return jwt.sign(
+        {id: this._id},
+        process.env.JWT_SECRET ,
+        {expiresIn: process.env.JWT_EXPIRE_TIME})
+}
+export default mongoose.model("User",userSchema)
